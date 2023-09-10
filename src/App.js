29969789844e2fb68392cb98fe4627d8ad80f41c
xicosantos <francisco.s.santos77@gmail.com>
2023-09-10 18:59:18 +0100

    test push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+//Constant elements (header and footer); 
+import Header from './Components/Header/header';
+import Footer from './Components/Footer/Footer';
+
+//Elementos de rotas de links
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+//pages
+import Home from './Pages/Home';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+
+    return(
+        <h1>This is the header</h1>
+    )
+
+}
+
+export default Header; 
